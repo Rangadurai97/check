61b093ccbbdcb5362b65791ea1e5a30caea9e297
Rangadurai97 <duraivarman97@gmail.com>
2025-01-29 17:06:14 +0530

    update

--- src/main/java/com/check/Checkit.java
+++ src/main/java/com/check/Checkit.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Checkit {
 	@GetMapping("/getCheckit")
 	public String getCheckit() {
-		return "Checked Successfully";
+		return "Checked";
 	}
 
 }
